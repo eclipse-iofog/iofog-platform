b67e56349163dbe120ef1f687e68ca7710ba2b9b
Pixcell <adewergifosse@gmail.com>
2019-07-08 17:26:26 +1200

    Update install docker script

--- scripts/credentials.template.sh
+++ scripts/credentials.template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PACKAGE_CLOUD_TOKEN=<package_cloud_token> # If you need to download private packages from packagecloud
-export PACKET_AUTH_TOKEN=<packet_auth_token> # If you want to deploy agents on Packet
-export GOOGLE_APPLICATION_CREDENTIALS=<path-to-json>
+export PACKAGE_CLOUD_TOKEN=YOUR_PACKAGE_CLOUD_TOKEN # If you need to download private packages from packagecloud
+export PACKET_AUTH_TOKEN=YOUR_PACKET_API_KEY # If you want to deploy agents on Packet
+export GOOGLE_APPLICATION_CREDENTIALS=YOUR_PATH_TO_JSON
