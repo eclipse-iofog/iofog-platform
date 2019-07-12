4fab18ffed2b5dc34b144ab40744fdd2701a7282
Pixcell <adewergifosse@gmail.com>
2019-07-12 20:51:14 +1200

    ignore empty followed by comment

--- scripts/generate_terraform_main.sh
+++ scripts/generate_terraform_main.sh
@@ -8,7 +8,7 @@ cp "$TERRAFORM_FOLDER/$MAIN_TERRAFORM_TEMPLATE" "$TERRAFORM_FOLDER/main.tf"
 
 # Check value of not commented 'packet_auth_token' variable
 # grep packet_auth_token, trim space and tabs, ignore lines commencing with #, split using =, get second value, trim "
-ENABLE_PACKET=$(cat $TERRAFORM_FOLDER/user_vars.tfvars | grep 'packet_auth_token' |  tr -d " " | tr -d "\t" | grep -v '^[#]' | awk -F '=' '{print $2}' | tr -d \")
+ENABLE_PACKET=$(cat $TERRAFORM_FOLDER/user_vars.tfvars | grep 'packet_auth_token' |  tr -d " " | tr -d "\t" | grep -v '^[#]' | awk -F '=' '{print $2}' | tr -d \" | grep -v '^[#]')
 
 if ! [[ -z $ENABLE_PACKET ]]; then
   cat "$TERRAFORM_FOLDER/$PACKET_TEMPLATE" >> "$TERRAFORM_FOLDER/main.tf"
