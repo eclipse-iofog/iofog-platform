c9ea9a126efac76da26615f106caa3a8b594b335
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-07-28 17:19:28 +0530

    fix the module names for outputs from root module

--- infrastructure/environments_gke/develop/outputs.tf
+++ infrastructure/environments_gke/develop/outputs.tf
@@ -1,38 +1,38 @@
 # VPC Outputs
 output "network_name" {
   description = "The name of the VPC being created"
-  value       = module.vpc.network_name
+  value       = module.gcp_network.network_name
 }
 
 output "routes" {
   description = "The routes associated with this VPC"
-  value       = module.vpc.routes
+  value       = module.gcp_network.routes
 }
 
 output "subnets_ips" {
   description = "The IPs and CIDRs of the subnets being created"
-  value       = module.vpc.subnets_ips
+  value       = module.gcp_network.subnets_ips
 }
 
 output "subnets_names" {
   description = "The names of the subnets being created"
-  value       = module.vpc.subnets_names
+  value       = module.gcp_network.subnets_names
 }
 
 output "subnets_secondary_ranges" {
   description = "The secondary ranges associated with these subnets"
-  value       = module.vpc.subnets_secondary_ranges
+  value       = module.gcp_network.subnets_secondary_ranges
 }
 
 # GKE
 output "name" {
     description = "Cluster name"
-    value       = "${module.gke.name}"
+    value       = "${module.kubernetes.name}"
 }
 
 output "region" {
     description = "Cluster region"
-    value       = "${module.gke.region}"
+    value       = "${module.kubernetes.region}"
 }
 
 # Packet ips
